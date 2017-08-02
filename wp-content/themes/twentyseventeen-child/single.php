4747f733592f63c58b98c05d4de3b3f182956b82
tatyanaberdan <tyb6@georgetown.edu>
2017-08-02 18:39:22 -0400

    adding code to get custom field to display

--- wp-content/themes/twentyseventeen-child/single.php
+++ wp-content/themes/twentyseventeen-child/single.php
@@ -30,10 +30,11 @@ get_header(); ?>
 				the_post_navigation( array(
 					'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'twentyseventeen' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '</span>%title</span>',
 					'next_text' => '<span class="screen-reader-text">' . __( 'Next Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'twentyseventeen' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ) . '</span></span>',
-				) );
-				echo get_post_meta($post->ID, 'Currently Reading', true);
+				) ); ?>
+				
+				<p>I am currently reading: <?php echo get_post_meta($post->ID, 'Currently Reading', true); ?></p>
 
-
+			<?php
 			endwhile; // End of the loop.
 			?>
 
