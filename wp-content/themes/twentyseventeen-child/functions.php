daef5782d1e379b6d2e6356f136051d5fc68b477
tatyanaberdan <tyb6@georgetown.edu>
2017-08-02 17:21:21 -0400

    fixing enqueue code

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -4,23 +4,22 @@ function my_theme_enqueue_styles() {
 
     $parent_style = 'twentyseventeen-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
 
-    wp_enqueue_style( $twentyseventeen_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
-        array( $twentyseventeen_style ),
+        array( $parent_style ),
         wp_get_theme()->get('Version')
     );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
-// creating The Loop
-define( 'WP_USE_THEMES', false ); get_header();
-if (have_posts()) :
-   while (have_posts()) :
-      the_post();
-         the_content();
-   endwhile;
-endif;
-get_sidebar();
-get_footer(); 
+add_action( 'loop_start', 'before_single_post_content' );
+function before_single_post_content() {
+if ( is_singular( 'post') ) {
+$cf = get_post_meta( get_the_ID(), 'custom_field_name', true );
+if( ! empty( $cf ) ) {
+echo '<div class="before-content">'. $cf .'</div>';
+    }
+  }
+}
 ?>
