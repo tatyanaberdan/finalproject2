0216183f53d3f4017f0a60477413bb026888362b
tatyanaberdan <tyb6@georgetown.edu>
2017-08-05 15:03:50 -0400

    edited enqueue code for style sheet

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -1,3 +1,4 @@
+
 <?php
 // enqueue 
 function my_theme_enqueue_styles() {
@@ -6,20 +7,10 @@ function my_theme_enqueue_styles() {
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . 'twentyseventeen-child/style.css',
+        get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-
-add_action( 'loop_start', 'before_single_post_content' );
-function before_single_post_content() {
-if ( is_singular( 'post') ) {
-$cf = get_post_meta( get_the_ID(), 'custom_field_name', true );
-if( ! empty( $cf ) ) {
-echo '<div class="before-content">'. $cf .'</div>';
-    }
-  }
-}
 ?>
