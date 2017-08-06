d7faaf875398f6370d2985e35c9667d9645d62f1
tatyanaberdan <tyb6@georgetown.edu>
2017-08-06 15:52:53 -0400

    attempt to get custom post type to appear in archive

--- wp-content/themes/twentyseventeen-child/archive.php
+++ wp-content/themes/twentyseventeen-child/archive.php
@@ -39,6 +39,16 @@ get_header(); ?>
 				 */
 				get_template_part( 'template-parts/post/content', get_post_format() );
 
+				function namespace_add_custom_types( $query ) {
+  					if( is_category() || is_tag() && empty( $query->query_vars['suppress_filters'] ) ) {
+   					 $query->set( 'post_type', array(
+     				'post', 'nav_menu_item', 'rl_bookreviews'
+						));
+	  				return $query;
+	}
+}
+add_filter( 'pre_get_posts', 'namespace_add_custom_types' );
+
 			endwhile;
 
 			the_posts_pagination( array(
