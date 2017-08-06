bd7fc7b4d46b25a39a8d8a64907eb305fa8ebfcb
tatyanaberdan <tyb6@georgetown.edu>
2017-08-05 21:21:44 -0400

    cleaning up code in book reviews custom post type
    adding credit comments and deleting extra comments

--- wp-content/plugins/bookreviews/bookreviews.php
+++ wp-content/plugins/bookreviews/bookreviews.php
@@ -10,9 +10,10 @@ Description: Review books I've read.
 Author: Tat'yana Berdan
 Version: 0.1
 Author URI: http://tatyanaberdan.com
+
+Code adapted from Andre Spittle's Book List plugin: https://github.com/andrewspittle/reading-list/blob/master/index.php
 */
 /**
- * Start up our custom post type and hook it in to the init action when that fires.
  *
  * @since bookreviews 0.1
  */
@@ -29,7 +30,6 @@ function rl_create_post_type() {
 		'new_item_name' 				=> __( 'New Book Review', 'bookreviews' ),
 		'menu_name' 					=> __( 'Book Reviews', 'bookreviews' ),
 	);
-	
 	$args = array (
 		'labels' 		=> $labels,
 		'public' 		=> true,
@@ -41,14 +41,11 @@ function rl_create_post_type() {
 	register_post_type( 'rl_bookreviews', $args );
 }
 /**
- * Create our custom taxonomies. One hierarchical one for genres and a flat one for authors.
  *
  * @since Reading List 0.1
  */
-/* Hook in to the init action and call rl_create_book_taxonomies when it fires. */
 add_action( 'init', 'rl_create_bookreviews_taxonomies', 0 );
 function rl_create_bookreviews_taxonomies() {
-	// Add new taxonomy, keep it non-hierarchical (like tags)
 	$labels = array(
 		'name' 							=> __( 'Authors', 'bookreviews' ),
 		'singular_name' 				=> __( 'Author', 'bookreviews' ),
@@ -62,7 +59,6 @@ function rl_create_bookreviews_taxonomies() {
 		'choose_from_most_used' 		=> __( 'Choose from the most used authors', 'bookreviews' ),
 		'menu_name' 					=> __( 'Authors', 'bookreviews' ),
 	); 	
-		
 	register_taxonomy( 'book-author', array( 'rl_bookreviews' ), array(
 		'hierarchical' 		=> false,
 		'labels' 			=> $labels,
@@ -71,7 +67,6 @@ function rl_create_bookreviews_taxonomies() {
 		'query_var' 		=> true,
 		'rewrite' 			=> array( 'slug' => 'book-author' ),
 	));
-
 	$labels = array(
 		'name' 							=> __( 'Genres', 'bookreviews' ),
 		'singular_name' 				=> __( 'Genre', 'bookreviews' ),
@@ -96,26 +91,20 @@ function rl_create_bookreviews_taxonomies() {
 }
 
 /**
- * Add custom meta box for tracking the page numbers of the book.
+ * Add custom meta box for tracking the rating and page numbers of the book.
  *
- * Props to Justin Tadlock: http://wp.smashingmagazine.com/2011/10/04/create-custom-post-meta-boxes-wordpress/
+ * Adapted from code by Justin Tadlock: http://wp.smashingmagazine.com/2011/10/04/create-custom-post-meta-boxes-wordpress/
  *
  * @since Reading List 1.0
  *
 */
-/* Fire our meta box setup function on the editor screen. */
 add_action( 'load-post.php', 'rl_post_meta_boxes_setup' );
 add_action( 'load-post-new.php', 'rl_post_meta_boxes_setup' );
-/* Our meta box set up function. */
 function rl_post_meta_boxes_setup() {
-	/* Add meta boxes on the 'add_meta_boxes' hook. */
 	add_action( 'add_meta_boxes', 'rl_add_post_meta_boxes' );
-	
-	/* Save post meta on the 'save_post' hook. */
 	add_action( 'save_post', 'rl_rating_save_meta', 10, 2 );
 	add_action( 'save_post', 'rl_pages_save_meta', 10, 2 );
 }
-/* Create one or more meta boxes to be displayed on the post editor screen. */
 function rl_add_post_meta_boxes() {
 	add_meta_box(
 		'rl-rating',								// Unique ID
@@ -134,7 +123,6 @@ function rl_add_post_meta_boxes() {
 		'default'								// Priority
 	);
 }
-/* Display the post meta box. */
 function rl_rating_meta_box( $object, $box ) { ?>
 
 	<?php wp_nonce_field( basename( __FILE__ ), 'rl_rating_nonce' ); ?>
@@ -148,54 +136,36 @@ function rl_pages_meta_box( $object, $box ) { ?>
 	<p><input class="widefat" type="text" name="rl-pages" id="rl-pages" value="<?php echo esc_attr( get_post_meta( $object->ID, 'rl_pages', true ) ); ?>" size="30" /></p>
 <?php }
 
-/* Save the meta box's data. */
 function rl_rating_save_meta( $post_id, $post ) {
-	/* Verify the nonce before proceeding. */
 	if ( !isset( $_POST['rl_rating_nonce'] ) || !wp_verify_nonce( $_POST['rl_rating_nonce'], basename( __FILE__ ) ) )
 		return $post_id;
-	/* Get the post type object. */
 	$post_type = get_post_type_object( $post->post_type );
-	/* Check if the current user has permission to edit the post. */
 	if ( !current_user_can( $post_type->cap->edit_post, $post_id ) )
 		return $post_id;
-	/* Get the posted data and sanitize it for use as an HTML class. */
 	$new_meta_value = ( isset( $_POST['rl-rating'] ) ? sanitize_html_class( $_POST['rl-rating'] ) : '' );
-	/* Get the meta key. */
 	$meta_key = 'rl_rating';
-	/* Get the meta value of the custom field key. */
 	$meta_value = get_post_meta( $post_id, $meta_key, true );
-	/* If a new meta value was added and there was no previous value, add it. */
 	if ( $new_meta_value && '' == $meta_value )
 		add_post_meta( $post_id, $meta_key, $new_meta_value, true );
-	/* If the new meta value does not match the old value, update it. */
 	elseif ( $new_meta_value && $new_meta_value != $meta_value )
 		update_post_meta( $post_id, $meta_key, $new_meta_value );
-	/* If there is no new meta value but an old value exists, delete it. */
 	elseif ( '' == $new_meta_value && $meta_value )
 		delete_post_meta( $post_id, $meta_key, $meta_value );
 } 
+
 function rl_pages_save_meta( $post_id, $post ) {
-	/* Verify the nonce before proceeding. */
 	if ( !isset( $_POST['rl_pages_nonce'] ) || !wp_verify_nonce( $_POST['rl_pages_nonce'], basename( __FILE__ ) ) )
 		return $post_id;
-	/* Get the post type object. */
 	$post_type = get_post_type_object( $post->post_type );
-	/* Check if the current user has permission to edit the post. */
 	if ( !current_user_can( $post_type->cap->edit_post, $post_id ) )
 		return $post_id;
-	/* Get the posted data and sanitize it for use as an HTML class. */
 	$new_meta_value = ( isset( $_POST['rl-pages'] ) ? sanitize_html_class( $_POST['rl-pages'] ) : '' );
-	/* Get the meta key. */
 	$meta_key = 'rl_pages';
-	/* Get the meta value of the custom field key. */
 	$meta_value = get_post_meta( $post_id, $meta_key, true );
-	/* If a new meta value was added and there was no previous value, add it. */
 	if ( $new_meta_value && '' == $meta_value )
 		add_post_meta( $post_id, $meta_key, $new_meta_value, true );
-	/* If the new meta value does not match the old value, update it. */
 	elseif ( $new_meta_value && $new_meta_value != $meta_value )
 		update_post_meta( $post_id, $meta_key, $new_meta_value );
-	/* If there is no new meta value but an old value exists, delete it. */
 	elseif ( '' == $new_meta_value && $meta_value )
 		delete_post_meta( $post_id, $meta_key, $meta_value ); } 
 	?>
