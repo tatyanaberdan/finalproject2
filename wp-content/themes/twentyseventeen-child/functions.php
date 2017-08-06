ffd8ff15c85eba0978033175bb758f3382e40016
tatyanaberdan <tyb6@georgetown.edu>
2017-08-06 15:32:30 -0400

    tried a different group of code to get custom post type to appear in blog

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -14,11 +14,14 @@ function my_theme_enqueue_styles() {
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 ?>
 <?php
-// adding custom post type to regular RSS feed to get custom post type to show up in blog - code adapted from http://www.wpbeginner.com/wp-tutorials/how-to-add-custom-post-types-to-your-main-wordpress-rss-feed/
-function myfeed_request($qv) {
-    if (isset($qv['feed']))
-        $qv['post_type'] = array('rl_bookreviews');
-    return $qv;
+// adding custom post type to regular RSS feed to get custom post type to show up in blog - code adapted from http://www.wpbeginner.com/wp-tutorials/how-to-add-custom-post-types-to-your-main-wordpress-rss-feed/ (https://css-tricks.com/snippets/wordpress/make-archives-php-include-custom-post-types/)
+function namespace_add_custom_types( $query ) {
+  if( is_category() || is_tag() && empty( $query->query_vars['suppress_filters'] ) ) {
+    $query->set( 'post_type', array(
+     'post', 'nav_menu_item', 'rl_bookreviews'
+        ));
+      return $query;
+    }
 }
-add_filter('request', 'myfeed_request'); 
+add_filter( 'pre_get_posts', 'namespace_add_custom_types' );
 ?>
