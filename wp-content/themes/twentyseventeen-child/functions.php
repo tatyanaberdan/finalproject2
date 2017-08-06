400357dfc4b7e8dbaaf53ce18c12d30f35bedf57
tatyanaberdan <tyb6@georgetown.edu>
2017-08-05 21:23:07 -0400

    cleaning up code in functions.php of child theme

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -2,7 +2,7 @@
 // enqueue 
 function my_theme_enqueue_styles() {
 
-    $parent_style = 'twentyseventeen-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $parent_style = 'twentyseventeen-style';
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
