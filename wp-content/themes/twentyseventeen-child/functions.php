4f75f6935e6f91a6502b90aeb8144095f20d9074
tatyanaberdan <tyb6@georgetown.edu>
2017-08-06 17:45:42 -0400

    added code to make book reviews appear in RSS feed

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -4,24 +4,21 @@ function my_theme_enqueue_styles() {
 
     $parent_style = 'twentyseventeen-style';
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
+    wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('child-style',
         get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
+        array($parent_style),
         wp_get_theme()->get('Version')
     );
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 ?>
 <?php
-// adding custom post type to regular RSS feed to get custom post type to show up in blog - code adapted from http://www.wpbeginner.com/wp-tutorials/how-to-add-custom-post-types-to-your-main-wordpress-rss-feed/ (https://css-tricks.com/snippets/wordpress/make-archives-php-include-custom-post-types/)
-function namespace_add_custom_types( $query ) {
-  if( is_category() || is_tag() && empty( $query->query_vars['suppress_filters'] ) ) {
-    $query->set( 'post_type', array(
-     'post', 'nav_menu_item', 'rl_bookreviews'
-        ));
-      return $query;
-    }
+// adding custom post type to regular RSS feed to get custom post type to show up in blog - code adapted from http://justintadlock.com/archives/2010/02/02/showing-custom-post-types-on-your-home-blog-page
+add_filter('pre_get_posts', 'my_get_posts');
+function my_get_posts($query) {
+    if ((is_home() && $query->is_main_query()) || is_feed())
+        $query->set('post_type', array( 'post', 'rl_bookreviews'));
+    return $query;
 }
-add_filter( 'pre_get_posts', 'namespace_add_custom_types' );
 ?>
