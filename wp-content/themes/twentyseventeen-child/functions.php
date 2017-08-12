f0f299f2884d292ee90620e3a67df9fc1378806b
tatyanaberdan <tyb6@georgetown.edu>
2017-08-11 20:51:35 -0400

    removing code (not working)

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -15,16 +15,10 @@ add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 ?>
 <?php
 // adding custom post type to regular RSS feed to get custom post type to show up in blog - code adapted from http://justintadlock.com/archives/2010/02/02/showing-custom-post-types-on-your-home-blog-page
-//add_filter('pre_get_posts', 'my_get_posts');
-//function my_get_posts($query) {
-    //if ((is_home() && $query->is_main_query()) || is_feed())
-        //$query->set('post_type', array( 'post', 'rl_bookreviews'));
-    //return $query;
-add_action('pre_get_posts', 'query_post_type');
-function query_post_type($query) {
-  if($query->is_main_query()
-    && ( is_category() || is_tag() )) {
-        $query->set( 'post_type', array('post','rl_bookreivews') );
-  }
+add_filter('pre_get_posts', 'my_get_posts');
+function my_get_posts($query) {
+    if ((is_home() && $query->is_main_query()) || is_feed())
+        $query->set('post_type', array( 'post', 'rl_bookreviews'));
+    return $query;
 }
 ?>
